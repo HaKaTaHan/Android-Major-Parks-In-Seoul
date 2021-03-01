176e6f2442931aceda61fbcac9cf52f844c9940d
Kim <rnlcksgrp1@naver.com>
2021-03-02 01:12:21 +0900

    ADD Splash ADD Main

---
+++ app/src/main/java/com/teamnoyes/majorparksinseoul/splash/SplashFragment.kt
@@ -0,0 +1,35 @@
+package com.teamnoyes.majorparksinseoul.splash
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.teamnoyes.majorparksinseoul.R
+import com.teamnoyes.majorparksinseoul.databinding.SplashFragmentBinding
+
+class SplashFragment : Fragment() {
+    private lateinit var splashFragmentBinding: SplashFragmentBinding
+    private lateinit var splashViewModel: SplashViewModel
+    private lateinit var splashViewModelFactory: SplashViewModelFactory
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        splashFragmentBinding = DataBindingUtil.inflate(inflater, R.layout.splash_fragment, container, false)
+
+        return splashFragmentBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val application = requireNotNull(this.activity).application
+        splashViewModelFactory = SplashViewModelFactory(application)
+        splashViewModel = ViewModelProvider(this, splashViewModelFactory).get(SplashViewModel::class.java)
+
+    }
+
+}
---
+++ app/src/main/java/com/teamnoyes/majorparksinseoul/splash/SplashViewModel.kt
@@ -0,0 +1,8 @@
+package com.teamnoyes.majorparksinseoul.splash
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class SplashViewModel(application: Application) : AndroidViewModel(application) {
+    // TODO: Implement the ViewModel
+}
